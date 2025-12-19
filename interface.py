13937611a7de35b5fc15fead49008491418cf78c
Jacopo <jacopo.024@gmail.com>
2025-12-19 13:02:17 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -80,12 +80,16 @@ nome = st.text_input("Insert your name *", key="name")
 cognome = st.text_input("Insert your surname *", key="surname")
 email = st.text_input("Insert your email *", key="email")
 st.write("Fields with * are obligatory")
+col1, col2, col3 = st.columns([1, 2, 1])
+with col1:
 buttonr_clicked = st.button("Delete Email", key="buttonr")
 if buttonr_clicked:
       if email != "":
               df = df[df['Email'] != email].reset_index(drop=True)
               upload_to_s3(df) # Scrivo tutto in memoria e sovrascrivo su S3
               st.success("Success")
+with col3:
+   st.write("Press only in case of sign out from the app *")
 else:  
       st.divider()
       if email in df["Email"].values:
@@ -322,6 +326,7 @@ else:
      
 
      
+
 
 
 
